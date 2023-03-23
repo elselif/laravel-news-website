6f40435e263f22f4e0d77ecb65b229e721d800f1
Elif <2003013054@student.beykent.edu.tr>
2023-03-24 02:57:44 +0300

    admin profile & image update commit 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,5 +29,12 @@ class AdminController extends Controller
     public function AdminLogoutPage()
     {
         return view('admin.admin_logout');
+    } //end method
+
+    public function AdminProfile()
+    {
+        $id= Auth::user()->id;
+        $adminData= User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
     } //end method
 }
