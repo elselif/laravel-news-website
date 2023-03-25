ad863552380f9480401ee33ca7f29696a92a9a7d
Elif <2003013054@student.beykent.edu.tr>
2023-03-25 03:17:32 +0300

    adding toastr in for view message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -59,7 +59,12 @@ class AdminController extends Controller
 
         $data->save();
 
-        return redirect()->back();
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
 
     } //end method
 
