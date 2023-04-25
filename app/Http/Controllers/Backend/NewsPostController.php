54d15c84797675b8dda153c76141424973a93c38
Elif <2003013054@student.beykent.edu.tr>
2023-04-26 02:12:37 +0300

    add news post sayfasını oluşturma

--- app/Http/Controllers/Backend/NewsPostController.php
+++ app/Http/Controllers/Backend/NewsPostController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\NewsPost;
 
 class NewsPostController extends Controller
 {
     public function AllNewsPost()
     {
-        return view('backend.news.news_post');
+        $allnews = NewsPost::latest()->get(); //NewsPost bir model sınıfıdır ve news_posts tablosuna karşılık gelir. latest() yöntemi, en son oluşturulan haberleri en üstte olacak şekilde sıralar. get() yöntemi ise sorguyu yürütür ve sonucu bir koleksiyon olarak döndürür.
+        return view('backend.news.all_news_post', compact('allnews'));
     } //end method
 }
