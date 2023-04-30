10c9c0775f014ea382b3d53d210407ad03d7ac5d
Elif <2003013054@student.beykent.edu.tr>
2023-04-30 03:18:10 +0300

    frontend show category

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -1,10 +1,13 @@
+@php
+    $cdate = new DateTime();
+@endphp
 <header class="themesbazar_header">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-4">
                 <div class="date">
                     <i class="lar la-calendar"></i>
-                    Dhaka, Saturday, 10th September 2022
+                    {{$cdate ->format('l d-m-Y')}}
                 </div>
             </div>
             <div class="col-lg-4 col-md-4">
@@ -78,13 +81,17 @@
                                     class="fa-solid fa-house-user"></i> Home</a>
                         </li>
                         
-                        <li id="menu-item-277"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-277">
-                            <a href=" ">International</a>
-                        </li>
+                        @php
+                            $categories = App\Models\Category::orderBy('category_name', 'ASC')->limit(7)->get();
+                        @endphp
+
+                        @foreach ($categories as $category)
+                            
+                        
+                       
                         <li id="menu-item-291"
                             class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-291 has-sub">
-                            <a href=" ">Sports</a>
+                            <a href=" ">{{$category->category_name}}</a>
                             <ul class="sub-menu">
                                 <li id="menu-item-294"
                                     class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-294">
@@ -94,65 +101,18 @@
                                     class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-292">
                                     <a href=" ">Sub Sports</a>
                                 </li>
-                                <li id="menu-item-293"
-                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-293">
-                                    <a href=" ">Sub Sports</a>
-                                </li>
-                                <li id="menu-item-295"
-                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-295">
-                                    <a href=" ">Sub Sports</a>
-                                </li>
+                                
 
                             </ul>
                             <a class="dd-toggle" href=" "><span class="icon-plus"></span></a>
                         </li>
-                        <li id="menu-item-270"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-270">
-                            <a href=" ">Opinion</a>
-                        </li>
-                        <li id="menu-item-287"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-287">
-                            <a href=" ">Business</a>
-                        </li>
-                        <li id="menu-item-267"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-267">
-                            <a href=" ">Youth</a>
-                        </li>
-                        <li id="menu-item-274"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-274">
-                            <a href=" ">Entertainment</a>
-                        </li>
-                        <li id="menu-item-284"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-284">
-                            <a href=" ">Lifestyle</a>
-                        </li>
-                        <li id="menu-item-278"
-                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-278">
-                            <a href=" ">EDUCATION </a>
-                        </li>
+                        @endforeach
+                        <li id="menu-item-277"
+                        class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-277">
+                        <a href=" ">ARCHIVE</a>
+                    </li>
 
-                        <li id="menu-item-301"
-                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-301 has-sub">
-                            <a href=" ">SCI-TECH</a>
-                            <ul class="sub-menu">
-                                <li id="menu-item-269"
-                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-269">
-                                    <a href=" ">SCI-TECH</a>
-                                </li>
-                                <li id="menu-item-273"
-                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-273">
-                                    <a href=" ">SCI-TECH</a>
-                                </li>
-                                <li id="menu-item-275"
-                                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-275">
-                                    <a href=" ">SCI-TECH</a>
-                                </li>
-
-
-
-                            </ul>
-                            <a class="dd-toggle" href=" "><span class="icon-plus"></span></a>
-                        </li>
+                        
                     </ul>
                 </div>
             </div>
